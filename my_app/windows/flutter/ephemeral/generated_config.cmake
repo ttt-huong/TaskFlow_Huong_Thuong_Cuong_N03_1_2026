2b224c825f2b65fcc1e8191959c47cb808e3ed58
Thương <151871361+thuong325@users.noreply.github.com>
2026-05-27 20:10:59 +0700

    fix gitignore

---
+++ my_app/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\LTDD\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\LTDD\\TaskFlow_Huong_Thuong_Cuong_N03_1_2026\\my_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\LTDD\\flutter"
+  "PROJECT_DIR=C:\\LTDD\\TaskFlow_Huong_Thuong_Cuong_N03_1_2026\\my_app"
+  "FLUTTER_ROOT=C:\\LTDD\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\LTDD\\TaskFlow_Huong_Thuong_Cuong_N03_1_2026\\my_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\LTDD\\TaskFlow_Huong_Thuong_Cuong_N03_1_2026\\my_app"
+  "FLUTTER_TARGET=C:\\LTDD\\TaskFlow_Huong_Thuong_Cuong_N03_1_2026\\my_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuNg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZGI1MGUyMDE2OA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NDI1Y2ZiNTRkMA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS40"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\LTDD\\TaskFlow_Huong_Thuong_Cuong_N03_1_2026\\my_app\\.dart_tool\\package_config.json"
+)
